ab9c84c629306d0a30ffda7240d871fb57189bc1
Celudirosa <cluqued@live.com>
2023-03-31 19:25:03 +0200

    💄 styles

--- wp-content/themes/beta/footer.php
+++ wp-content/themes/beta/footer.php
@@ -1,3 +1,65 @@
+      <!-- FOOTER -->
+      <footer>
+      <img src="imgs/logo_dark-bg_small.svg" alt="logo fluorescen-T" />
+      <p>Be Fluorescen-T</p>
+      <div class="footer-line"></div>
+      <ul class="footer-nav">
+        <li><a href="#">Product</a></li>
+        <li><a href="#">Applications</a></li>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <div class="footer-line"></div>
+      <p class="footer-connected">Stay connected</p>
+      <ul class="footer-social">
+        <li>
+          <a href="https://www.youtube.com/">
+            <img src="imgs/icon_youtube.svg" alt="youtube" title="???" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/">
+            <img src="imgs/icon_facebook.svg" alt="facebook" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/">
+            <img src="imgs/icon_instagram.svg" alt="instagram" />
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/home?lang=es">
+            <img src="imgs/icon_twitter.svg" alt="twitter" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.tiktok.com/es/">
+            <img src="imgs/icon_tiktok.svg" alt="tiktok" />
+          </a>
+        </li>
+      </ul>
+      <div class="footer-line"></div>
+      <div class="footer-pay">
+        <img src="imgs/icon_visa.svg" alt="icon visa" />
+        <img src="imgs/icon_mastercard.svg" alt="icon mastercard" />
+        <img src="imgs/icon_paypal.svg" alt="icon paypal" />
+      </div>
+      <div class="footer-line"></div>
+      <ul class="footer-nav-terms">
+        <li>
+          <a href="#">Terms & conditions</a>
+        </li>
+        <li>
+          <a href="#">Privacy policy</a>
+        </li>
+        <li>
+          <a href="#">Cookie policy</a>
+        </li>
+      </ul>
+      <div class="footer-line"></div>
+      <p class="footer-copy">© 2023 Fluorescen-T</p>
+    </footer>
+
   <?php wp_footer(); ?>
   </body>
 </html> 
---
+++ wp-content/themes/beta/home.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<!-- MAIN -->
+
+<?php get_footer(); ?>
